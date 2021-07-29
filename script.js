bdf917fd2b669fca29b8dd3bec93fb8cd22d7e43
Liz Ireland <liz.t.ireland@gmail.com>
2021-07-28 19:15:39 -0700

    will the files update? Hopefully!

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
-var brewerydbUrl = "https://api.openbrewerydb.org/breweries?by_city=san_diego&by_dist=38.9877,77.0365"
+var tableBody = document.getElementById('repo-table');
+var fetchButton = document.getElementById('fetch-button')
+var cityNameEl = document.getElementById("city-name")
 
-function getBrewery(){
-    var brewerydbUrl ="https://api.openbrewerydb.org/breweries?by_city=san_diego&by_dist=38.9877,77.0365";
-    fetch(brewerydbUrl)
+function getCityName(event) {
+  event.preventDefault();
+  var cityName = cityNameEl.value
+  cityName = cityName.replace(" ", "_").toLowerCase()
+  console.log(cityName)
+  getApi(cityName)
+}
+
+function getApi(city) {
+ 
+    var requestUrl = "https://api.openbrewerydb.org/breweries?by_city="+ city;
+    fetch(requestUrl)
     .then(function (response) {
         return response.json();
-      })
-      .then(function (data) {
+    })
+    .then(function(data){
         console.log(data)
-      })
-
-    };
+        for (var i = 0; i < data.length; i++) {
+            var createTableRow = document.createElement("tr");
+            var tableData = document.createElement("td");
+            var link = document.createElement("p");
+            link.textContent = data[i].name;
+            tableData.appendChild(link);
+            createTableRow.appendChild(tableData);
+            tableBody.appendChild(createTableRow);
+        }
+    }).catch(function(err){
+      console.log(err)
+    })
+}
+fetchButton.addEventListener("click", getCityName);
+fetchButton.addEventListener("keydown", function(event){
+event.preventDefault();
+if (event.key === "Enter") {
+     getCityName()
+   } else {
+     return event
+   }
+ })
