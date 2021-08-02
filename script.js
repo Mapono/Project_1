f1ef0d01ef744d57f225b201c9f4d593eea3aec7
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-08-02 11:36:31 -0700

    change event listener for add buttons and set items to local storage

--- script.js
+++ script.js
@@ -12,62 +12,63 @@ function getCityName(event) {
 }
 
 function getApi(city) {
- 
-    var requestUrl = "https://api.openbrewerydb.org/breweries?by_city="+ city;
-    fetch(requestUrl)
+
+  var requestUrl = "https://api.openbrewerydb.org/breweries?by_city=" + city;
+  fetch(requestUrl)
     .then(function (response) {
-        return response.json();
+      return response.json();
     })
-    .then(function(data){
+    .then(function (data) {
       console.log(data)
       for (var i = 0; i < data.length; i++) {
-          var createTableRow = document.createElement("tr");
-          var tableData = document.createElement("td");
-          var text = document.createElement("h4");
-          text.textContent = data[i].name;
-          tableData.appendChild(text);
-          createTableRow.appendChild(tableData);
-          tableBody.appendChild(createTableRow);
-          var createTableRow = document.createElement("tr");
-          var tableData = document.createElement("td");
-          var text = document.createElement("p");
-          text.textContent = data[i].street;
-          tableData.appendChild(text);
-          createTableRow.appendChild(tableData);
-          tableBody.appendChild(createTableRow);
-          var createTableRow = document.createElement("tr");
-          var tableData = document.createElement("td");
-          var text = document.createElement("p");
-          text.textContent = data[i].city;
-          tableData.appendChild(text);
-          createTableRow.appendChild(tableData);
-          tableBody.appendChild(createTableRow);
-          var createTableRow = document.createElement("tr");
-          var tableData = document.createElement("td");
-          var text = document.createElement("p");
-          text.textContent = data[i].website_url;
-          tableData.appendChild(text);
-          createTableRow.appendChild(tableData);
-          tableBody.appendChild(createTableRow);
-          // add button
-          var addButton = document.createElement("button");
-          addButton.innerHTML = '<i class="fa fa-plus-square" aria-hidden="true"></i>';
-          addButton.classList.add("add-button");
-          createTableRow.appendChild(addButton);
+        var createTableRow = document.createElement("tr");
+        var tableData = document.createElement("td");
+        var text = document.createElement("h4");
+        text.textContent = data[i].name;
+        tableData.appendChild(text);
+        createTableRow.appendChild(tableData);
+        tableBody.appendChild(createTableRow);
+        var createTableRow = document.createElement("tr");
+        var tableData = document.createElement("td");
+        var text = document.createElement("p");
+        text.textContent = data[i].street;
+        tableData.appendChild(text);
+        createTableRow.appendChild(tableData);
+        tableBody.appendChild(createTableRow);
+        var createTableRow = document.createElement("tr");
+        var tableData = document.createElement("td");
+        var text = document.createElement("p");
+        text.textContent = data[i].city;
+        tableData.appendChild(text);
+        createTableRow.appendChild(tableData);
+        tableBody.appendChild(createTableRow);
+        var createTableRow = document.createElement("tr");
+        var tableData = document.createElement("td");
+        var text = document.createElement("p");
+        text.textContent = data[i].website_url;
+        tableData.appendChild(text);
+        createTableRow.appendChild(tableData);
+        tableBody.appendChild(createTableRow);
+        
+        // add button
+        var addButton = document.createElement("button");
+        addButton.innerHTML = '<i class="fa fa-plus-square" aria-hidden="true"></i>';
+        addButton.setAttribute("class", "addbutton");
+        createTableRow.appendChild(addButton);
+
+        let save = data[i];
+        // console.log(save);
+        function saveBrewery() {
+          var saved = JSON.parse(localStorage.getItem("savedBrews")) || [];
+          saved.push(save);
+          localStorage.setItem("savedBrews", JSON.stringify(saved));
+        };
+        addButton.addEventListener("click", function () { saveBrewery(save) });
       }
-  })
-}
+    })
+};
 
 fetchButton.addEventListener("click", getCityName);
 
-var addButton = document.querySelectorAll(".add-button");
 
-// create saveBrewery
-function saveBrewery(event){
-  event.preventDefault();
-  console.log("clicked");
-};
-saveBrewery();
-// save brewery when click add button FOR EVERY BUTTON
-var addButtons = document.querySelectorAll("add-button");
-addButtons = document.addEventListener("click", saveBrewery);
+
