ecd3a1fe59c1a4b5d4f38e4eee447fe772837e41
Liz Ireland <liz.t.ireland@gmail.com>
2021-08-02 18:28:33 -0700

    updating Javascript and HTML files

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var tableBody = document.getElementById('repo-table');
 var fetchButton = document.getElementById('fetch-button')
 var cityNameEl = document.getElementById("city-name")
 var breweryAddressEl = document.getElementById("brewery-address")
+var breweryCardContainer = document.querySelector(".breweries-container")
 
 function getCityName(event) {
   event.preventDefault();
@@ -21,39 +22,30 @@ function getApi(city) {
     .then(function(data){
       console.log(data)
       for (var i = 0; i < data.length; i++) {
-          var createTableRow = document.createElement("tr");
-          var tableData = document.createElement("td");
+          var createBreweryCardEl = document.createElement("div");
+          createBreweryCardEl.classList.add("brewery-card");
+          var breweryData = document.createElement("p");
           var text = document.createElement("h4");
           text.textContent = data[i].name;
-          tableData.appendChild(text);
-          createTableRow.appendChild(tableData);
-          tableBody.appendChild(createTableRow);
-          var createTableRow = document.createElement("tr");
-          var tableData = document.createElement("td");
+          createBreweryCardEl.appendChild(text);
+          createBreweryCardEl.appendChild(breweryData);
+          text.textContent = data[i].city;
+          var breweryData = document.createElement("p");
           var text = document.createElement("p");
           text.textContent = data[i].street;
-          tableData.appendChild(text);
-          createTableRow.appendChild(tableData);
-          tableBody.appendChild(createTableRow);
-          var createTableRow = document.createElement("tr");
-          var tableData = document.createElement("td");
+          createBreweryCardEl.appendChild(text);
+          createBreweryCardEl.appendChild(breweryData);
+          var breweryData = document.createElement("p");
           var text = document.createElement("p");
           text.textContent = data[i].city;
-          tableData.appendChild(text);
-          createTableRow.appendChild(tableData);
-          tableBody.appendChild(createTableRow);
-          var createTableRow = document.createElement("tr");
-          var tableData = document.createElement("td");
-          var text = document.createElement("p");
-          text.textContent = data[i].website_url;
-          tableData.appendChild(text);
-          createTableRow.appendChild(tableData);
-          tableBody.appendChild(createTableRow);
+          createBreweryCardEl.appendChild(text);
+          createBreweryCardEl.appendChild(breweryData);
           // add button
           var addButton = document.createElement("button");
           addButton.innerHTML = '<i class="fa fa-plus-square" aria-hidden="true"></i>';
           addButton.classList.add("add-button");
-          createTableRow.appendChild(addButton);
+          createBreweryCardEl.appendChild(addButton);
+          breweryCardContainer.appendChild(createBreweryCardEl)
       }
   })
 }
@@ -67,7 +59,6 @@ function saveBrewery(event){
   event.preventDefault();
   console.log("clicked");
 };
-saveBrewery();
 // save brewery when click add button FOR EVERY BUTTON
 var addButtons = document.querySelectorAll("add-button");
 addButtons = document.addEventListener("click", saveBrewery);
